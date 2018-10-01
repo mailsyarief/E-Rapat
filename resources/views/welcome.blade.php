49dd43eff7fabbfb268b1c3b0a0b9150e152f0f4
mailsyarief <ismail8syarief@gmail.com>
2018-10-01 21:31:36 +0700

    stylesheet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,14 +19,14 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<link rel="stylesheet" href="css/lib/lobipanel/lobipanel.min.css">
-{{-- <link rel="stylesheet" href="{{ asset('css/lib/lobipanel/lobipanel.min.css') }}"> --}}
-<link rel="stylesheet" href="css/separate/vendor/lobipanel.min.css">
-<link rel="stylesheet" href="css/lib/jqueryui/jquery-ui.min.css">
-<link rel="stylesheet" href="css/separate/pages/widgets.min.css">
-<link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
-<link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
-<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/lib/lobipanel/lobipanel.min.css">
+    {{-- <link rel="stylesheet" href="{{ asset('css/lib/lobipanel/lobipanel.min.css') }}"> --}}
+    <link rel="stylesheet" href="css/separate/vendor/lobipanel.min.css">
+    <link rel="stylesheet" href="css/lib/jqueryui/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/separate/pages/widgets.min.css">
+    <link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
 
 </head>
 <body class="with-side-menu control-panel control-panel-compact">
